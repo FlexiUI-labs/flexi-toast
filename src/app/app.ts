020c9238ab5c0362a6675411c7adda297e94f0b2
Taner Saydam <taner_and_you@hotmail.com>
2025-06-15 00:00:38 +0300

    I updated README

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlexiToastOptionsModel, FlexiToastService } from 'flexi-toast';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  template:`
+  <div style="margin: 30px; display:flex; flex-direction: column; gap: 5px; width:200px">
+    <button style="background-color:#47D764; color: white; padding:10px" (click)="success()">Success</button>
+    <button style="background-color:#2F86EB; color: white; padding:10px" (click)="info()">Info</button>
+    <button style="background-color:#FFC021; color: black; padding:10px" (click)="warning()">Warning</button>
+    <button style="background-color:#ff355b; color: white; padding:10px" (click)="error()">Error</button>
+    <button style="padding:10px" (click)="swal()">Swal</button>
+  </div>
+  `
 })
 export class App {
-  protected title = 'flexi-toast';
+  readonly options = signal<FlexiToastOptionsModel>(new FlexiToastOptionsModel());
+
+  readonly #toast = inject(FlexiToastService);
+
+  constructor(){
+    this.options.update(prev => ({
+      ...prev,
+      autoClose: true,
+      position: 'bottom-right',
+      themeClass: 'light'
+    }))
+  }
+
+  success(){
+    this.#toast.showToast("Success","This is a success message", "success", this.options());
+  }
+
+  info(){
+    this.#toast.showToast("Info","This is a info message", "info", this.options());
+  }
+
+  warning(){
+    this.#toast.showToast("Warning","This is a warning message", "warning", this.options());
+  }
+
+  error(){
+    this.#toast.showToast("Error","This is a error message", "error", this.options());
+  }
+
+  swal(){
+    this.#toast.showSwal("Question?","This is a question?","Yes",() => {
+      this.#toast.showToast("Info","This is a info message", "info");
+    },"No")
+  }
 }
